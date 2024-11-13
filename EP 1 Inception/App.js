647b28d98d9cb93a3c438e1410c9889904a62aa2
Pradeep Detroja <detrojapradeep111@gmail.com>
2024-11-13 16:58:57 +0000

    React Hello World, React CDN

---
+++ EP 1 Inception/App.js
@@ -0,0 +1,28 @@
+/* 
+
+<div id="parent">
+    <div id="child1">
+        <h1>I am H1</h1>
+        <h2>I am H2</h2>
+    </div>
+    <div id="child2">
+        <h1>I am H1</h1>
+        <h2>I am H2</h2>
+    </div>
+</div> 
+
+*/
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I am H1"),
+    React.createElement("h2", {}, "I am H2"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am H1"),
+    React.createElement("h2", {}, "I am H2"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
---
+++ EP 1 Inception/README.md
@@ -0,0 +1,5 @@
+# This is the episode 1 of the Namaste React
+
+# Learnt about frameworks, libraries, React CDN, Create element and root.
+
+# Learnt about creating a hello world in React!
