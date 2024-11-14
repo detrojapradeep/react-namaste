99ef1c9958eed6fbc01c7cca7a3dc681ce140546
Pradeep Detroja <detrojapradeep111@gmail.com>
2024-11-14 12:04:54 +0000

    added React, ReactDOM, Parcel dev and prod build, dist, parcel-cache

--- EP 02 Igniting our App/App.js
+++ EP 02 Igniting our App/App.js
@@ -3,3 +3,14 @@
 // ~ tilde means do it for major updgrades
 
 // Package lock. json keeps track of the exact dependency installed with SHA Hash codes for validity
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "I am a react element"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
