c4ba8e0b39e58863902d589f5655f2d1f1b1fc90
Pradeep Detroja <detrojapradeep111@gmail.com>
2024-11-14 15:36:40 +0000

    JSX Introduction

--- EP 02 Igniting our App/App.js
+++ EP 02 Igniting our App/App.js
@@ -1,16 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 // Package.json is configuration for NPM
 // ^ caret means do automatic minor upgrades for the package
 // ~ tilde means do it for major updgrades
 
 // Package lock. json keeps track of the exact dependency installed with SHA Hash codes for validity
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "I am a react element"
-);
+const heading = <h1 id="heading"> This is a new JSX Tag</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
