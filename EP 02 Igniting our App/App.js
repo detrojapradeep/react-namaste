b665b95cb856f716163f889cca3fbba0d6b638a6
Pradeep Detroja <detrojapradeep111@gmail.com>
2024-11-14 11:07:51 +0000

    added npm, Parcel, package.json, package-lock.json, node_modules

---
+++ EP 02 Igniting our App/App.js
@@ -0,0 +1,5 @@
+// Package.json is configuration for NPM
+// ^ caret means do automatic minor upgrades for the package
+// ~ tilde means do it for major updgrades
+
+// Package lock. json keeps track of the exact dependency installed with SHA Hash codes for validity
